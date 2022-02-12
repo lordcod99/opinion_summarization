73eb76a94bc0478f0036566003fffe6c0fe61696
kzyzahk <kzyzahk@gmail.com>
2022-02-12 19:50:33 +0530

    test

--- summirization/extractive.py
+++ summirization/extractive.py
@@ -4,6 +4,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+# Git Hub
 
 def tok(data):
 

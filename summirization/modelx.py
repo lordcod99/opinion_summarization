d3930f64cdfaa69b7ebd0d7a0ac20b8668f258d2
sai <lordcodeking@gmail.com>
2022-04-08 02:13:40 +0530

    path scoring nad stem and lemma

--- summirization/modelx.py
+++ summirization/modelx.py
@@ -1,8 +1,9 @@
+#basic model
+import re
+import socketserver
 import nltk
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import wordnet
+from numpy import true_divide
 
-# 
 
 class Node:
 
@@ -13,7 +14,44 @@ class Node:
         self.id = [[sid,pid]]
         self.link = []
 
-f = open('file.txt','r')
+
+# ven=['.',',',"but",]
+
+def is_valid_start(psi):
+    d=4
+    s=0
+    for i in  psi:
+        s+=i[1]
+    s=s/len(psi)
+    if(s<=d):
+        return True
+    else:
+        return False
+
+
+def is_valid_end(word):
+    ven=['.',',',"but","yet"]
+    if ven.count(word)>0:
+        return True
+    else:
+        return False
+
+def find_path(wp,word ,info ,score ,path_len):
+    sentance = word
+    rdncy=len(info.id)
+    dr=3
+    if rdncy>= dr:
+        if(is_valid_end(word)):
+            return sentance
+    for ngb in info.link:
+        path_len += 1
+        score += 1
+        sentance+=ngb
+        find_path(wp,ngb,wp[ngb],score,path_len)
+
+
+
+f = open('file.txt','r')   
 v = []
 
 for x in f:
@@ -25,12 +63,12 @@ f.close()
 
 
 
-cc = 0
+# cc = 0
 lp = {}
 wp ={}
 
-for x in v:
-    for y in x: print(y[0])
+# for x in v:
+#     for y in x: print(y[0])
 
 for x in range(len(v)):
     lp[x+1] = Node(x+1,"PO",x+1,0)
@@ -40,7 +78,22 @@ for x in range(len(v)):
         if y == 0: lp[x+1].link.append(v[x][y][0])
         else: wp[v[x][y-1][0]].link.append(v[x][y][0])
 
-for x in wp: print(wp[x].name,'\t',wp[x].id,'\t',wp[x].link)
+# for x in wp: print(wp[x].name,'\t',wp[x].id,'\t',wp[x].link)
+
+
+# finding best paths
+
+summary = []
+
+for word , info in wp.items():
+    if(is_valid_start(info.id)):
+        path_len=1
+        score =0
+        summary.append(find_path(wp,word,info,score,path_len))
+
+
+
+
 
 
 
